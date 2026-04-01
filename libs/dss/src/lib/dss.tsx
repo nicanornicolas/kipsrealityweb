e3bcdfa57b0daf0ff532e6f045dcecd2a4519769
nicanornicolas <nicanornicolas4@mylife.mku.ac.ke>
2026-04-01 10:19:45 +0300

    feat(dss): add new DSS library with React component, tests, and module exports

---
+++ libs/dss/src/lib/dss.tsx
@@ -0,0 +1,11 @@
+import styles from './dss.module.css';
+
+export function Dss() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Dss!</h1>
+    </div>
+  );
+}
+
+export default Dss;
