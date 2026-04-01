93de924328594e3990a07c3991f7bc4ebf6ff7f3
nicanornicolas <nicanornicolas4@mylife.mku.ac.ke>
2026-04-01 10:28:30 +0300

    feat(payments): add initial payments component
    Add new React component, test file, and CSS module for the payments feature. This establishes the foundational structure to support future payments functionality development.

---
+++ libs/payments/src/lib/payments.tsx
@@ -0,0 +1,11 @@
+import styles from './payments.module.css';
+
+export function Payments() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Payments!</h1>
+    </div>
+  );
+}
+
+export default Payments;
