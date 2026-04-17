b1aa40a259d24a1a0dfe92d167b6bf91e5db118a
nicanornicolas <nicanornicolas4@mylife.mku.ac.ke>
2026-04-17 18:22:53 +0300

    build(background-worker): add built utilities library files
    This commit adds the compiled JavaScript files and source maps for the utilities library to the background-worker's dist folder. The built files are generated from the TypeScript source in `libs/utilities/src/` and are required for the background-worker to use the utilities library at runtime.

---
+++ dist/apps/background-worker/libs/utilities/src/index.js
@@ -0,0 +1,61 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __reExport = (target, mod, secondTarget) => (__copyProps(target, mod, "default"), secondTarget && __copyProps(secondTarget, mod, "default"));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var src_exports = {};
+__export(src_exports, {
+  UtilityService: () => import_utility_service.UtilityService
+});
+module.exports = __toCommonJS(src_exports);
+var import_utility_service = require("./lib/utility-service");
+__reExport(src_exports, require("./lib/utility-ai-job-store"), module.exports);
+__reExport(src_exports, require("./lib/utility-allocation-service"), module.exports);
+__reExport(src_exports, require("./lib/utility-bill-service"), module.exports);
+__reExport(src_exports, require("./lib/utility-posting-service"), module.exports);
+__reExport(src_exports, require("./lib/utility-reading-service"), module.exports);
+__reExport(src_exports, require("./lib/utility-types"), module.exports);
+__reExport(src_exports, require("./lib/utility-validators"), module.exports);
+__reExport(src_exports, require("./lib/notifications/notification-service"), module.exports);
+__reExport(src_exports, require("./lib/notifications/sms-factory"), module.exports);
+__reExport(src_exports, require("./lib/utilities"), module.exports);
+__reExport(src_exports, require("./lib/queue"), module.exports);
+__reExport(src_exports, require("./lib/dss-queue"), module.exports);
+__reExport(src_exports, require("./lib/mail"), module.exports);
+__reExport(src_exports, require("./lib/webhook-processors"), module.exports);
+__reExport(src_exports, require("./lib/notification-service"), module.exports);
+__reExport(src_exports, require("./lib/outbound-webhook"), module.exports);
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  UtilityService,
+  ...require("./lib/utility-ai-job-store"),
+  ...require("./lib/utility-allocation-service"),
+  ...require("./lib/utility-bill-service"),
+  ...require("./lib/utility-posting-service"),
+  ...require("./lib/utility-reading-service"),
+  ...require("./lib/utility-types"),
+  ...require("./lib/utility-validators"),
+  ...require("./lib/notifications/notification-service"),
+  ...require("./lib/notifications/sms-factory"),
+  ...require("./lib/utilities"),
+  ...require("./lib/queue"),
+  ...require("./lib/dss-queue"),
+  ...require("./lib/mail"),
+  ...require("./lib/webhook-processors"),
+  ...require("./lib/notification-service"),
+  ...require("./lib/outbound-webhook")
+});
+//# sourceMappingURL=index.js.map
