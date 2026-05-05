6f3119d168e75ceca465c6960454798e5c02ba47
Nicanor Nicolas <93964167+nicanornicolas@users.noreply.github.com>
2026-05-05 18:55:35 +0300

    fix: correct import path for `cn` utility in FormInput component

--- src/components/ui/form-input.tsx
+++ src/components/ui/form-input.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { cn } from './';
+import { cn } from '@/lib/utils';
 import { AlertCircle } from 'lucide-react';
 
 interface FormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
